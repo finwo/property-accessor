d540bfd432e5557c8b5ea56a8b847bd0f927da63
Robin Bron <admin@finwo.nl>
2016-04-13 01:37:25 +0200

    Added get methods for objects

--- src/PropertyAccessor.php
+++ src/PropertyAccessor.php
@@ -4,6 +4,13 @@ namespace Finwo\PropertyAccessor;
 
 class PropertyAccessor
 {
+    protected $debug = false;
+
+    public function __construct($debug = false)
+    {
+        $this->debug          = $debug;
+    }
+
     /**
      * @param        $subject
      * @param string $path
@@ -14,15 +21,13 @@ class PropertyAccessor
      */
     public function get($subject, $path = '', $pathSplit = '|')
     {
-        //split the path
-        $path = explode($pathSplit, $path);
-
-        //try array
+        // try array for legacy mapper
         if (is_array($subject)) {
+            $path = explode($pathSplit, $path);
             return $this->getArrayProperty($subject, $path);
         }
 
-        //throw error if needed
+        // throw error if needed
         if (!is_object($subject)) {
             throw new \Exception(sprintf(
                 'Subject must be an array or object, %s given',
@@ -30,7 +35,46 @@ class PropertyAccessor
             ));
         }
 
-        //all methods failed, throw exception
+        $camelized = $this->camelize($path);
+
+        // try the default getter
+        if (method_exists($subject, $method = sprintf("get%s", $camelized))) {
+            return call_user_func(array(
+                $subject,
+                $method
+            ));
+        }
+
+        // try less-common getter
+        if (method_exists($subject, $method = lcfirst($camelized))) {
+            return call_user_func(array(
+                $subject,
+                $method
+            ));
+        }
+
+        // try generic getter
+        if (method_exists($subject, 'get')) {
+            return $subject->get($path);
+        }
+
+        // try generic hidden getter
+        if (method_exists($subject, '__get')) {
+            return $subject->__get($path);
+        }
+
+        // try fetching directly
+        $rc = new \ReflectionObject($subject);
+        foreach ($rc->getProperties(\ReflectionProperty::IS_PUBLIC) as $property) {
+            if ($property->getName() == $path) {
+                return $subject->{$path};
+            }
+        }
+
+        // all methods failed, throw exception
+        if ($this->getDebug()) {
+            return '#ERROR';
+        }
         throw new \Exception(sprintf(
             'Required property "%s" of class %s is missing in data',
             '',
@@ -49,15 +93,13 @@ class PropertyAccessor
      */
     public function set(&$subject, $path = '', $value, $pathSplit = '|')
     {
-        //split the path
-        $path = explode($pathSplit, $path);
-
-        //try array
+        // try array for legacy mapper
         if (is_array($subject)) {
+            $path = explode($pathSplit, $path);
             return $this->setArrayProperty($subject, $path, $value);
         }
 
-        //throw error if needed
+        // throw error if needed
         if (!is_object($subject)) {
             throw new \Exception(sprintf(
                 'Subject must be an array or object, %s given',
@@ -65,7 +107,11 @@ class PropertyAccessor
             ));
         }
 
-        //all methods failed, throw exception
+        // all methods failed, throw exception
+        if ($this->getDebug()) {
+            return '#ERROR';
+        }
+
         throw new \Exception(sprintf(
             'Required property "%s" of class %s is missing in data',
             '',
@@ -94,6 +140,23 @@ class PropertyAccessor
             $target = &$target[$key];
         }
         $target = $value;
+
         return $this;
+    }
+
+    /**
+     * Simple camelize function
+     *
+     * @param string $scored
+     * @return mixed
+     */
+    protected function camelize( $scored = '' ) {
+        $output = str_replace(array('-', '_'), " ", $scored );
+        return str_replace(' ','',ucwords($output));
+    }
+
+    protected function getDebug()
+    {
+        return $this->debug;
     }
 }
